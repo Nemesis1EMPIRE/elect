f43ca657067bda0c6b48742018704297820a0acf
Nemesis1EMPIRE <137032212+Nemesis1EMPIRE@users.noreply.github.com>
2025-01-29 20:30:29 +0100

    Update videoscreen.dart

--- lib/screens/videoscreen.dart
+++ lib/screens/videoscreen.dart
@@ -1,55 +1,64 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
-import 'package:chewie/chewie.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
-class ChewieVideoPlayer extends StatefulWidget {
-  final String videoPath;
-  const ChewieVideoPlayer({super.key, required this.videoPath});
+class VideoScreen extends StatefulWidget {
+  const VideoScreen({super.key});
 
   @override
-  _ChewieVideoPlayerState createState() => _ChewieVideoPlayerState();
+  _VideoScreenState createState() => _VideoScreenState();
 }
 
-class _ChewieVideoPlayerState extends State<ChewieVideoPlayer> {
-  late VideoPlayerController _videoPlayerController;
-  late ChewieController _chewieController;
-  bool _isInitialized = false;
+class _VideoScreenState extends State<VideoScreen> {
+  late VideoPlayerController _controller;
+  String? videoPath; // 📌 Stocke le chemin local de la vidéo
 
   @override
   void initState() {
     super.initState();
-    _videoPlayerController = VideoPlayerController.asset(widget.videoPath);
+    _loadVideo();
+  }
 
-    _videoPlayerController.initialize().then((_) {
-      setState(() {
-        _chewieController = ChewieController(
-          videoPlayerController: _videoPlayerController,
-          autoPlay: false,
-          looping: false,
-          aspectRatio: _videoPlayerController.value.aspectRatio,
-          showControls: true,
-        );
-        _isInitialized = true;
-      });
+  Future<void> _loadVideo() async {
+    // 📌 Copier la vidéo depuis les assets vers le stockage temporaire
+    final tempDir = await getTemporaryDirectory();
+    final tempVideoFile = File("${tempDir.path}/video.mp4");
+
+    if (!await tempVideoFile.exists()) {
+      final byteData = await rootBundle.load("assets/vid/video.mp4");
+      await tempVideoFile.writeAsBytes(byteData.buffer.asUint8List(), flush: true);
+    }
+
+    setState(() {
+      videoPath = tempVideoFile.path;
+      _controller = VideoPlayerController.file(tempVideoFile)
+        ..initialize().then((_) {
+          setState(() {});
+          _controller.play();
+        });
     });
   }
 
   @override
   void dispose() {
-    _videoPlayerController.dispose();
-    _chewieController.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Chewie Video Player")),
-      body: Center(
-        child: _isInitialized
-            ? Chewie(controller: _chewieController)
-            : const CircularProgressIndicator(),
-      ),
+      appBar: AppBar(title: const Text("Lecture Vidéo")),
+      body: videoPath == null
+          ? const Center(child: CircularProgressIndicator())
+          : Center(
+              child: AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              ),
+            ),
     );
   }
 }
