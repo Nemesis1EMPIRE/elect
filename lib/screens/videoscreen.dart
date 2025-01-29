8c9b2cc082f15e6cea2102abe3c71e572c66463b
Nemesis1EMPIRE <137032212+Nemesis1EMPIRE@users.noreply.github.com>
2025-01-29 18:32:52 +0100

    Update videoscreen.dart

--- lib/screens/videoscreen.dart
+++ lib/screens/videoscreen.dart
@@ -1,90 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
 
-class VideoScreen extends StatefulWidget {
-  const VideoScreen({super.key});
+class ChewieVideoPlayer extends StatefulWidget {
+  final String videoPath;
+  const ChewieVideoPlayer({super.key, required this.videoPath});
 
   @override
-  _VideoScreenState createState() => _VideoScreenState();
+  _ChewieVideoPlayerState createState() => _ChewieVideoPlayerState();
 }
 
-class _VideoScreenState extends State<VideoScreen> {
-  final List<String> videoPaths = [
-    'assets/vid/video1.mp4',
-    'assets/vid/video.mp4',
-  ];
-
-  late List<VideoPlayerController> _controllers;
-  late List<Future<void>> _initializeControllers;
+class _ChewieVideoPlayerState extends State<ChewieVideoPlayer> {
+  late VideoPlayerController _videoPlayerController;
+  late ChewieController _chewieController;
+  bool _isInitialized = false;
 
   @override
   void initState() {
     super.initState();
+    _videoPlayerController = VideoPlayerController.asset(widget.videoPath);
 
-    // Initialisation des contrôleurs vidéo
-    _controllers = videoPaths.map((path) => VideoPlayerController.asset(path)).toList();
-    _initializeControllers = _controllers.map((controller) {
-      return controller.initialize().then((_) {
-        setState(() {}); // 📌 Rafraîchit l'interface après initialisation
+    _videoPlayerController.initialize().then((_) {
+      setState(() {
+        _chewieController = ChewieController(
+          videoPlayerController: _videoPlayerController,
+          autoPlay: false,
+          looping: false,
+          aspectRatio: _videoPlayerController.value.aspectRatio,
+          showControls: true,
+        );
+        _isInitialized = true;
       });
-    }).toList();
-
-    // Activer la lecture automatique en boucle
-    for (var controller in _controllers) {
-      controller.setLooping(true);
-      controller.setVolume(1.0);
-    }
+    });
   }
 
   @override
   void dispose() {
-    for (var controller in _controllers) {
-      controller.dispose();
-    }
+    _videoPlayerController.dispose();
+    _chewieController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Lecteur Vidéo Défilant'),
-        backgroundColor: Colors.blueAccent,
-      ),
-      body: FutureBuilder(
-        future: Future.wait(_initializeControllers), // 📌 Attendre l'initialisation
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return PageView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: _controllers.length,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _controllers[index].value.isPlaying
-                          ? _controllers[index].pause()
-                          : _controllers[index].play();
-                    });
-                  },
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      AspectRatio(
-                        aspectRatio: _controllers[index].value.aspectRatio,
-                        child: VideoPlayer(_controllers[index]),
-                      ),
-                      if (!_controllers[index].value.isPlaying)
-                        const Icon(Icons.play_circle_fill, size: 80, color: Colors.white),
-                    ],
-                  ),
-                );
-              },
-            );
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
+      appBar: AppBar(title: const Text("Chewie Video Player")),
+      body: Center(
+        child: _isInitialized
+            ? Chewie(controller: _chewieController)
+            : const CircularProgressIndicator(),
       ),
     );
   }
