0028727e61950cb233ab1aeca2da77d65d1a916d
Nemesis1EMPIRE <137032212+Nemesis1EMPIRE@users.noreply.github.com>
2025-02-08 16:40:01 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:elect241/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Elect241App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
